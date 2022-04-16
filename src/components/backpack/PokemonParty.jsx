74ec7fab567137295462bce2b0a56fae54506176
viktorHek <viktorkarlsson125@gmail.com>
2022-04-16 23:39:56 +0200

    display pokemons look like shit

--- src/components/backpack/PokemonParty.jsx
+++ src/components/backpack/PokemonParty.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import pokemons from '../../database/pokemons'
 
 const PokemonParty = () => {
 	const { myPokemons } = useSelector((state) => state)
-	const populatedPartyList = []
-	let count = 0
+	const [pokeParty, setPokeParty] = useState([])
+	const [pokemonPartyRender, setPokemonPartyRender] = useState(false)
 
 	useEffect(() => {
 		populatePokemonParty()
@@ -13,30 +13,41 @@ const PokemonParty = () => {
 	}, [myPokemons])
 
 	function populatePokemonParty() {
-		console.log('@populatePokemonParty!!')
+		let populatedPartyList = []
 		for (let index = 0; index < 6; index++) {
 			if (myPokemons[index]) {
 				pokemons.forEach((el) => {
-					if(el.id === myPokemons[index].id) {
+					if (el.id === myPokemons[index].id) {
 						populatedPartyList.push(el)
 					}
 				})
 			}
 		}
-		console.log({populatedPartyList})
+		setPokeParty(populatedPartyList)
+		console.log({pokeParty})
+		setPokemonPartyRender(true)
+		console.log({pokemonPartyRender})
 	}
-	
-  // let pokemonList = populatedPartyList.map((el) => {
+
+	// let pokemonList = populatedPartyList.map((el) => {
 	// 	console.log('in map!!',{el})
-  //   count = count + 1
-  //   return (
+	//   count = count + 1
+	//   return (
 	// 		<div key={`pokemonParty-${count}`}>
 	// 			<p>
 	// 				{el.name}
 	// 			</p>
 	// 		</div>
 	// 	)
-  // })
+	// })
+
+	let pokemonList = pokeParty.map((el, index) => {
+		console.log({el})
+		return <div key={index}>{el.name}</div>;
+	})
+
+
+
 
 	// console.log('pokemonList',pokemonList)
 
@@ -44,16 +55,7 @@ const PokemonParty = () => {
 		<div className='main_pokemon_party_container'>
 			{/* <p>Här är mina pokemons</p> */}
 			<div >
-				{/* {pokemonList && pokemonList} */}
-				{populatedPartyList.length > 0 ? (
-            populatedPartyList.map((el, index) => {
-              return <div key={index}>{el.name}</div>;
-            })
-          ) : (
-            <div>
-              You have no Pokemons {populatedPartyList.length}
-            </div>
-          )}
+				{pokemonPartyRender && pokemonList}
 			</div>
 		</div>
 	)
