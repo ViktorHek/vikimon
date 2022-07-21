72af9dcbbd3d82ac259f3d0de226264f07e511b7
viktorHek <viktorkarlsson125@gmail.com>
2022-07-21 12:16:40 +0200

    adding battle interface backgrond

--- src/views/Fight.jsx
+++ src/views/Fight.jsx
@@ -6,11 +6,32 @@ const Fight = () => {
   // useEffect(() => {
   //   setMapPos(playermovement.map)
   // },[playermovement])
+  let battleBackgrond = '/images/backgronds/battle/battleInterface_grass1.jpg'
 
   return (
-    <div style={{position: 'relative', top: '0px', left: '0px'}}>
-      <div style={{width: '100%', height: '100%', backgroundColor: 'blue'}}>
-        hello
+    <div style={{position: 'relative', top: '0px', left: '0px', height: '100%'}}>
+      <div
+        style={{
+          position: 'relative',
+          top: '0px',
+          left: '0px',
+          height: '450px',
+          width: '500px',
+          overflow: 'hidden',
+        }}
+      >
+        <img
+          src={battleBackgrond}
+          alt="error"
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            objectFit: 'fill'
+          }}
+        />
       </div>
     </div>
   )
