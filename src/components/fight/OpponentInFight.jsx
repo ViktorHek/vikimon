8ecfe2e476e3ba8308d0147952687edc8e3a894e
viktorHek <viktorkarlsson125@gmail.com>
2022-09-21 00:33:53 +0200

    fighting opponent mon sprite looking good

--- src/components/fight/OpponentInFight.jsx
+++ src/components/fight/OpponentInFight.jsx
@@ -9,9 +9,11 @@ const Fight = () => {
     //   const [attacks, setAttacks] = useState([])
     const { selectedAttackFronRedux } = useSelector((state) => state)
 
-    let fullPokemonStripte = '/images/pokemons/Game Boy GBC - Pokemon Red Blue - Pokemon Color Front.png'
-    let posX = 65
-    let posY = 29
+    let fullPokemonStripte = '/images/pokemons/spr_green-supgb_001_transparent.png'
+    let posX = 0
+    let posY = 0
+    // let posX = 65
+    // let posY = 29
 
     useEffect(() => {
         console.log('update in opponentInFigt', selectedAttackFronRedux)
@@ -21,8 +23,8 @@ const Fight = () => {
         <div className='fight-opponent-main-container'>
             <div className='fight-opponent-mon-img-container'>
                 <img src={fullPokemonStripte} alt='pokemon' style={{
-                    height: '917px',
-                    width: '1727px',
+                    height: '100%',
+                    width: '100%',
                     position: 'absolute',
                     top: `-${posY}px`,
                     left: `-${posX}px`
